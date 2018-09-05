f8b0ca1128818648c3624365747bce45685f8c21
Sohini Paul <sohini.paul@seppmed.de>
2018-09-05 11:46:02 +0200

    jenkinsproject

---
+++ testwithjenkins.java
@@ -0,0 +1,14 @@
+
+public class testwithjenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+    	for(i=0;i<10;i++) {
+        System.out.println( "Hello World!" );
+        System.out.println( "First Maven Project!" );
+    	}
+
+	}
+
+}
